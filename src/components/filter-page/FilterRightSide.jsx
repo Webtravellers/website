a289a66a8b6f4c6b2409d2d95d7f45557ea7ca3e
hymena <61942901+hymena@users.noreply.github.com>
2022-06-23 17:00:07 +0300

    filtreleme sayfası dinamik
    Veriler veritabanından çekildi

--- src/components/filter-page/FilterRightSide.jsx
+++ src/components/filter-page/FilterRightSide.jsx
@@ -1,77 +1,27 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useNavigate } from "react-router"
+import LocationService from "../../services/locationService"
 
 const FilterRightSide = () => {
     const navigate = useNavigate()
+    const [locations, setLocations] = useState([])
 
+    useEffect(() => {
+        const locationService = new LocationService()
+        locationService.getLocations().then(res => {
+            setLocations(res.data.data)
+        })
+    })
     return (
-        <div className="d-flex flex-row my-filter-rightside">
-            <div className="d-flex flex-column align-items-center mr-5">
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image20.png")} alt="" />
-                    <span>Place 1</span>
+        <div className="my-grid-cols-4 m-5 p-5">
+            {locations.map(location => (
+                <div className="d-flex flex-column justify-content-center align-items-center bg-white rounded mb-0 mb-5">
+                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay wh-200-150" src={location.photos[0]} alt="" />
+                    <p className="text-center">{location.name}</p>
                 </div>
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image21.png")} alt="" />
-                    <span>Place 2</span>
-                </div>
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image22.png")} alt="" />
-                    <span>Place 3</span>
-                </div>
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image23.png")} alt="" />
-                    <span>Place 4</span>
-                </div>
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image25.png")} alt="" />
-                    <span>Place 5</span>
-                </div>
-            </div>
-            <div className="d-flex flex-column align-items-center mr-5">
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image20.png")} alt="" />
-                    <span>Place 1</span>
-                </div>
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image21.png")} alt="" />
-                    <span>Place 2</span>
-                </div>
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image22.png")} alt="" />
-                    <span>Place 3</span>
-                </div>
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image23.png")} alt="" />
-                    <span>Place 4</span>
-                </div>
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image25.png")} alt="" />
-                    <span>Place 5</span>
-                </div>
-            </div>
-            <div className="d-flex flex-column align-items-center">
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image20.png")} alt="" />
-                    <span>Place 1</span>
-                </div>
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image21.png")} alt="" />
-                    <span>Place 2</span>
-                </div>
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image22.png")} alt="" />
-                    <span>Place 3</span>
-                </div>
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image23.png")} alt="" />
-                    <span>Place 4</span>
-                </div>
-                <div className="d-flex flex-column align-items-center bg-white rounded mb-0 mb-5">
-                    <img onClick={() => { navigate("/location") }} className="m-3 cursor-pointer hover-overlay" src={require("../../assets/imgs/image25.png")} alt="" />
-                    <span>Place 5</span>
-                </div>
-            </div>
+            ))}
+
+
         </div>
     )
 }
