b9261a4f67bdf04532f619256dfd5192ea323d30
AlifurkanKaraman <82064494+AlifurkanKaraman@users.noreply.github.com>
2022-08-04 13:32:42 +0300

    Update google maps api key

--- src/pages/TripPage.jsx
+++ src/pages/TripPage.jsx
@@ -6,72 +6,67 @@ import LocationService from "../services/locationService";
 import TripService from "../services/tripService";
 import { BiMap } from "../utils/map";
 
-
 const TripPage = () => {
+  const { tripId } = useParams();
+  const {
+    user: { _id: userId },
+  } = useSelector((state) => state.auth);
+  const [trip, setTrip] = useState({});
+  const [locations, setLocations] = useState([]);
+  const tripService = new TripService();
+  const locationService = new LocationService();
+  const [loading, setLoading] = useState(true);
 
-    const { tripId } = useParams()
-    const { user: { _id: userId } } = useSelector((state) => state.auth);
-    const [trip, setTrip] = useState({})
-    const [locations, setLocations] = useState([])
-    const tripService = new TripService()
-    const locationService = new LocationService()
-    const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    if (!loading) return;
+    tripService.getTripByTripId(userId, tripId).then((res) => {
+      setLoading(false);
+      setTrip(res.data.data);
+      console.log(res.data.data);
+    });
+    locationService.getLocations().then((res) => {
+      setLocations(res.data.data);
+      console.log(res.data.data);
+    });
+  }, [loading]);
 
-    useEffect(() => {
-        if (!loading) return
-        tripService.getTripByTripId(userId, tripId).
-            then(res => {
-                setLoading(false)
-                setTrip(res.data.data)
-                console.log(res.data.data);
-            })
-        locationService.getLocations().then(res => {
-            setLocations(res.data.data)
-            console.log(res.data.data);
-        })
-
-
-    }, [loading])
-
-    useEffect(() => {
-        if (trip.locations) {
-            let map = new BiMap()
-            map.init(document.getElementById('routerMap'))
-            map.render()
-            if (trip.locations.length > 1)
-                map.calculateAndDisplayRoute(trip.locations.map(l => l.location.coordinates), BiMap.MODE.DRIVING)
-        }
-    }, [trip])
-
-    const handleAddLocation = (location) => {
-        setLoading(true)
-        tripService.addLocationToTrip(userId, tripId, { location })
-        console.log(userId, tripId, { location });
+  useEffect(() => {
+    if (trip.locations) {
+      let map = new BiMap();
+      map.init(document.getElementById("routerMap"));
+      if (trip.locations.length > 1)
+        map.calculateAndDisplayRoute(
+          trip.locations.map((l) => l.location.coordinates),
+          BiMap.MODE.DRIVING
+        );
     }
+  }, [trip]);
 
-    const handleRemoveLocation = (locationId) => {
-        setLoading(true)
-        tripService.removeLocationFromTrip(userId, tripId, locationId)
-    }
+  const handleAddLocation = (location) => {
+    setLoading(true);
+    tripService.addLocationToTrip(userId, tripId, { location });
+    console.log(userId, tripId, { location });
+  };
 
+  const handleRemoveLocation = (locationId) => {
+    setLoading(true);
+    tripService.removeLocationFromTrip(userId, tripId, locationId);
+  };
 
+  return (
+    <>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h3>{trip?.name}</h3>
+        <div>
+          <h4> Locations that already in your trip</h4>
+          {trip?.locations?.map((location) => (
+            <p>{location.name}</p>
+          ))}
+        </div>
 
-    return (
-        <>
-            <div className="d-flex flex-column justify-content-center align-items-center">
-                <h3>{trip?.name}</h3>
-                <div>
-                    <h4> Locations that already in your trip</h4>
-                    {
-                        trip?.locations?.map((location) => (
-                            <p>{location.name}</p>
-                        ))
-                    }
-                </div>
-
-                {/* <div>
-                    <h4> Browse All Locations To Add Your Trip</h4>
-                    {
+        <div>
+          <h4> Browse All Locations To Add Your Trip</h4>
+          {
                         locations?.map((location) => (
                             <div className="d-flex m-2 p-2 justify-content-center align-items-center">
                                 <p>{location?.name}</p>
@@ -83,14 +78,11 @@ const TripPage = () => {
                             </div>
                         ))
                     }
-                </div> */}
-                <div id="routerMap" >
+        </div>
+        <div id="routerMap"></div>
+      </div>
+    </>
+  );
+};
 
-                </div>
-            </div>
-        </>
-    )
-
-}
-
-export default TripPage
+export default TripPage;
--- src/utils/map.js
+++ src/utils/map.js
@@ -1,55 +1,51 @@
-const google = window.google
+const google = window.google;
 const directionsService = new google.maps.DirectionsService();
 const directionsRenderer = new google.maps.DirectionsRenderer();
-console.log(directionsRenderer)
-let map
-
+console.log(directionsRenderer);
+let map;
 
 export class BiMap {
-    constructor() {
+  constructor() {}
 
-    }
+  init(element) {
+    map = new google.maps.Map(element, {
+      zoom: 11,
+      center: { lat: 39.91, lng: 32.8 },
+    });
+    directionsRenderer.setMap(map);
+  }
 
-    init(element) {
-        map = new google.maps.Map(
-            element,
-            {
-                zoom: 6,
-                center: { lat: 41.85, lng: -87.65 },
-            }
-        );
-    }
+  calculateAndDisplayRoute(wayPoints, mode) {
+    const points = wayPoints.map((p) => {
+      return {
+        location: p.join(","),
+        stopover: true,
+      };
+    });
+    const opt = {
+      origin: wayPoints[0].join(","),
+      destination: wayPoints.at(-1).join(","),
+      waypoints: points,
+      optimizeWaypoints: true,
+      travelMode: mode,
+    };
+    console.log(opt);
+    directionsService
+      .route(opt)
+      .then((response) => {
+        console.log(response);
+        directionsRenderer.setDirections(response);
+        this.render();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    render() {
-        directionsRenderer.setMap(this.map)
-    }
-
-    calculateAndDisplayRoute(wayPoints, mode) {
-        const points = wayPoints.map(p => {
-            return {
-                location: p.join(","),
-                stopover: true
-            }
-        })
-        const opt = {
-            origin: wayPoints[0].join(","),
-            destination: wayPoints.at(-1).join(","),
-            waypoints: points,
-            optimizeWaypoints: true,
-            travelMode: mode
-        }
-        console.log(opt);
-        directionsService.route(opt).then((response) => {
-            directionsRenderer.setDirections(response);
-        }).catch(err => {
-            console.log(err);
-        })
-    }
-
-    static MODE = {
-        DRIVING: google.maps.TravelMode.DRIVING,
-        WALKING: google.maps.TravelMode.WALKING,
-        TRANSIT: google.maps.TravelMode.TRANSIT,
-        BICYCLING: google.maps.TravelMode.BICYCLING
-    }
+  static MODE = {
+    DRIVING: google.maps.TravelMode.DRIVING,
+    WALKING: google.maps.TravelMode.WALKING,
+    TRANSIT: google.maps.TravelMode.TRANSIT,
+    BICYCLING: google.maps.TravelMode.BICYCLING,
+  };
 }
