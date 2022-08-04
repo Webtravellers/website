cd778dfc239eeac8d477658ab86a0242c86dcdd7
AlifurkanKaraman <82064494+AlifurkanKaraman@users.noreply.github.com>
2022-08-04 17:33:29 +0300

    Missing div added

--- src/pages/TripPage.jsx
+++ src/pages/TripPage.jsx
@@ -6,81 +6,80 @@ import LocationService from "../services/locationService";
 import TripService from "../services/tripService";
 import { useTranslation } from "react-i18next";
 
-
 const TripPage = () => {
-    const { t, i18n } = useTranslation();
-    const { tripId } = useParams()
-    const { user: { _id: userId } } = useSelector((state) => state.auth);
-    const [trip, setTrip] = useState({})
-    const [locations, setLocations] = useState([])
-    const tripService = new TripService()
-    const locationService = new LocationService()
-    const [loading, setLoading] = useState(true)
+  const { t, i18n } = useTranslation();
+  const { tripId } = useParams();
+  const {
+    user: { _id: userId },
+  } = useSelector((state) => state.auth);
+  const [trip, setTrip] = useState({});
+  const [locations, setLocations] = useState([]);
+  const tripService = new TripService();
+  const locationService = new LocationService();
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        if (!loading) return
-        tripService.getTripByTripId(userId, tripId).
-            then(res => {
-                setLoading(false)
-                setTrip(res.data.data)
-                console.log(res.data.data);
-            })
-        locationService.getLocations().then(res => {
-            setLocations(res.data.data)
-            console.log(res.data.data);
-        })
-    }, [loading])
+  useEffect(() => {
+    if (!loading) return;
+    tripService.getTripByTripId(userId, tripId).then((res) => {
+      setLoading(false);
+      setTrip(res.data.data);
+      console.log(res.data.data);
+    });
+    locationService.getLocations().then((res) => {
+      setLocations(res.data.data);
+      console.log(res.data.data);
+    });
+  }, [loading]);
 
+  const handleAddLocation = (location) => {
+    setLoading(true);
+    tripService.addLocationToTrip(userId, tripId, { location });
+    console.log(userId, tripId, { location });
+  };
 
-    const handleAddLocation = (location) => {
-        setLoading(true)
-        tripService.addLocationToTrip(userId, tripId, { location })
-        console.log(userId, tripId, { location });
-    }
+  const handleRemoveLocation = (locationId) => {
+    setLoading(true);
+    tripService.removeLocationFromTrip(userId, tripId, locationId);
+  };
 
-    const handleRemoveLocation = (locationId) => {
-        setLoading(true)
-        tripService.removeLocationFromTrip(userId, tripId, locationId)
-    }
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <h3>{trip?.name}</h3>
+      <div>
+        <h4>{t("trip-page.h4-part1")}</h4>
+        {trip?.locations?.map((location) => (
+          <p>{location.name}</p>
+        ))}
+      </div>
 
+      <div>
+        <h4>{t("trip-page.h4-part2")}</h4>
 
+        {locations?.map((location) => (
+          <div className="d-flex m-2 p-2 justify-content-center align-items-center">
+            <p>{location?.name}</p>
+            {trip?.locations?.some((x) => x._id === location?._id) ? (
+              <Button
+                onClick={() => handleRemoveLocation(location._id)}
+                className="bg-danger"
+              >
+                {" "}
+                -{" "}
+              </Button>
+            ) : (
+              <Button
+                onClick={() => handleAddLocation(location._id)}
+                className="bg-success"
+              >
+                {" "}
+                +{" "}
+              </Button>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <>
-            <div className="d-flex flex-column justify-content-center align-items-center">
-                <h3>{trip?.name}</h3>
-                <div>
-                    <h4>{t("trip-page.h4-part1")}</h4>
-                    {
-                        trip?.locations?.map((location) => (
-                            <p>{location.name}</p>
-                        ))
-                    }
-                </div>
-
-                <div>
-                    <h4>{t("trip-page.h4-part2")}</h4>
-                {/* <div>
-                    <h4> Browse All Locations To Add Your Trip</h4>
-
-                    {
-                        locations?.map((location) => (
-                            <div className="d-flex m-2 p-2 justify-content-center align-items-center">
-                                <p>{location?.name}</p>
-                                {
-                                    trip?.locations?.some(x => x._id === location?._id) ? <Button onClick={() => handleRemoveLocation(location._id)} className="bg-danger"> - </Button>
-                                        : <Button onClick={() => handleAddLocation(location._id)} className="bg-success"> + </Button>
-                                }
-
-                            </div>
-                        ))
-                    }
-                </div> */}
-
-            </div>
-        </>
-    )
-
-}
-
-export default TripPage
+export default TripPage;
