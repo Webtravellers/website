5ab92c23e90a4b949e59f51b8577631fe77fac92
AlifurkanKaraman <82064494+AlifurkanKaraman@users.noreply.github.com>
2022-08-12 00:12:56 +0300

    update

--- src/components/home-page/ForgottenFlavors.jsx
+++ src/components/home-page/ForgottenFlavors.jsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ForgottenFlavorsCard from "./ForgottenFlavorsCard";
 import forgottenFlavors from "./forgottenFlavors";
+import LocationService from "../../services/locationService";
 
 function createCard(flavor) {
+    
     return(
         <ForgottenFlavorsCard 
-        key = {flavor.id}
+        key = {flavor._id}
         name = {flavor.name}
-        photos = {flavor.photos}
-        type = {flavor.type}
+        photos = {flavor.photos[0]}
+        type = {flavor.food-type}
         />
     )
 }
 
 function ForgottenFlavors() {
+    const [locations, setLocations] = useState([])
+    const unutulmuşLezzetler = []
+    useEffect(() => {
+        const locationService = new LocationService()
+        locationService.getDiscoverLocations({}).then(res => {
+            setLocations(res.data.data)
+        })
+    }, [])
+
+    const divideLocations = () => {
+        for (let i = 0; i <= locations.length; i++) {
+            if (locations[i]?.type[0]?.name === "Unutulmuş lezzetler") {
+                unutulmuşLezzetler.push(locations[i])
+            }
+        }
+    }
+    divideLocations()
+    console.log(unutulmuşLezzetler)
     return(
         <div className="forgotten-flavors d-flex flex-row">
-            {forgottenFlavors.map(createCard)}
+            {unutulmuşLezzetler.map(createCard)}
         </div>
     )
 }
