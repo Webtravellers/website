04524dc8bdc269c8f3c67820b8a31a97dd874bae
AlifurkanKaraman <82064494+AlifurkanKaraman@users.noreply.github.com>
2022-08-12 15:17:11 +0300

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,8 @@ import { CookieService, CookieTypes } from "./cookieService";
 
 export default () => {
   const api = axios;
-  axios.defaults.baseURL = "http://localhost:5000/";
+  
+  axios.defaults.baseURL = window.location.hostname.includes("localhost") ? "http://localhost:5000/" : "https://bihatiraapi.herokuapp.com/";
   axios.defaults.headers.common["Authorization"] ="Bearer " + CookieService.get(CookieTypes.AUTH)?.token;
   return api;
 };
